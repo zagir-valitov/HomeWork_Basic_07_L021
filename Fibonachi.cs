438f4158c549543e072a55f4451baeec8049a691
Загир Валитов <zagir-valitov@outlook.com>
2024-09-23 16:03:56 +0500

    Добавьте файлы проекта.

---
+++ Fibonachi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Fibonachi
+    {        
+        public static long RecursiveMethod(long Number)
+        {
+            //if (Number == 0 || Number == 1)
+            //{
+            //    return Number;
+            //}
+            return Number > 1 ? RecursiveMethod(Number - 1) + RecursiveMethod(Number - 2) : Number;
+        }
+
+
+        
+        public static long CyclicMethod(long Numbern)
+        {
+            long _base1 = 0;
+            long _base2 = 1;
+            long _temp;
+            for (long i = 0; i < Numbern; i++)
+            {
+                _temp = _base1;
+                _base1 = _base2;
+                _base2 += _temp;
+            }
+            return _base1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using Exercise_1;
+using System.Diagnostics;
+
+Console.WriteLine(" ---- Home work 7. Exercise 1 ----\n");
+
+long num1 = 5;
+long num2 = 10;
+long num3 = 20;
+
+Console.WriteLine("Time to calculate Fibonachi number by recursive method:");
+Console.WriteLine($"1. Fibonachi({num1}) = {TimeCalculateRecursiveMethod(num1).resultCalculate}," +
+    $"\t\tTime: {TimeCalculateRecursiveMethod(num1).time} ticks");
+Console.WriteLine($"1. Fibonachi({num2}) = {TimeCalculateRecursiveMethod(num2).resultCalculate}," +
+    $"\t\tTime: {TimeCalculateRecursiveMethod(num2).time} ticks");
+Console.WriteLine($"1. Fibonachi({num3}) = {TimeCalculateRecursiveMethod(num3).resultCalculate}," +
+    $"\tTime: {TimeCalculateRecursiveMethod(num3).time} ticks");
+
+Console.WriteLine("\nTime to calculate Fibonachi number by cyclic method:");
+Console.WriteLine($"1. Fibonachi({num1}) = {TimeCalculateCyclicMethod(num1).resultCalculate}," +
+    $"\t\tTime: {TimeCalculateCyclicMethod(num1).time} ticks");
+Console.WriteLine($"1. Fibonachi({num2}) = {TimeCalculateCyclicMethod(num2).resultCalculate}," +
+    $"\t\tTime: {TimeCalculateCyclicMethod(num2).time} ticks");
+Console.WriteLine($"1. Fibonachi({num3}) = {TimeCalculateCyclicMethod(num3).resultCalculate}," +
+    $"\tTime: {TimeCalculateCyclicMethod(num3).time} ticks");
+
+(long time, long resultCalculate) TimeCalculateRecursiveMethod(long number)
+{
+    var st = Stopwatch.StartNew();
+    long result = Fibonachi.RecursiveMethod(number);
+    st.Stop();
+    return (st.ElapsedTicks, result);
+
+}
+(long time, long resultCalculate) TimeCalculateCyclicMethod(long number)
+{
+    var st = Stopwatch.StartNew();
+    long result = Fibonachi.CyclicMethod(number);
+    st.Stop();
+    return (st.ElapsedTicks, result);
+}
